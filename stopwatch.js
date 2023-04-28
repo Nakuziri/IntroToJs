f3f269b6661da7a56058f61360ee45a2049b0016
Xavier Campos <xcampos@brooklynsteamcenter.org>
2023-04-28 14:54:54 -0400

    Base of stopwatch

---
+++ stopwatch.js
@@ -0,0 +1,11 @@
+function StartCount () {
+    alert("You clicked the start button")
+}
+
+function StopCount () {
+    alert("You clicked the stop button")
+}
+
+function ResetCount () {
+    alert("You clicked the reset button")
+}
