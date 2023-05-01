6475890c18df383d13f8188b87166b33ddcc9791
Xavier Campos <xcampos@brooklynsteamcenter.org>
2023-05-01 14:55:26 -0400

    Functions fixed and worked on design (somewhat)

--- stopwatch.js
+++ stopwatch.js
@@ -1,11 +1,40 @@
-function StartCount () {
-    alert("You clicked the start button")
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+
+let interval;
+
+function Start () {
+    Reset()
+
+    interval = setInterval(function (){
+        milliseconds = milliseconds + 10
+
+        if (milliseconds >= 1000) {
+            seconds = seconds + 1
+            milliseconds = 0
+        }
+
+
+
+        if (seconds >= 60) {
+            minutes = minutes + 1 
+            seconds = 0
+        }        
+        
+        digits.innerHTML = minutes + ':' + String(seconds).padStart(2, '0') + ':' +milliseconds
+    }, 10)
 }
 
-function StopCount () {
-    alert("You clicked the stop button")
+function Stop () {
+    clearInterval(interval)
 }
 
-function ResetCount () {
-    alert("You clicked the reset button")
+function Reset () {
+   stop()
+
+milliseconds = 0
+seconds = 0
+minutes = 0
+
 }
